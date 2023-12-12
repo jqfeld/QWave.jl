36e1c3879826dd129227c8701a6b3e26a5911d6b
Documenter.jl <documenter@juliadocs.github.io>
2023-12-12 14:00:56 +0000

    build based on 21051dd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QWave","category":"page"},{"location":"#QWave","page":"Home","title":"QWave","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QWave.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QWave]","category":"page"},{"location":"#QWave.QWave","page":"Home","title":"QWave.QWave","text":"Package to control QWave spectrometers via the ShortLink protocol\n\n\n\n\n\n","category":"module"}]
+}
